c8eaecd1330179660338d9c643f39027d23f04b9
Olga Ostapenko <olga.ostapenko@leverx.com>
2016-05-23 05:20:21 -0700

    Helper methods and classes created

---
+++ group.py
@@ -0,0 +1,11 @@
+__author__ = 'olga.ostapenko'
+
+
+class Group:
+
+    def __init__(self, name, header, footer):
+        self.name = name
+        self.header = header
+        self.footer = footer
+
+
