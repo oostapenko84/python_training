15178e8d869f13986f403a714f09f4636a431706
Olga Ostapenko <olga.ostapenko@leverx.com>
2016-05-31 08:33:57 -0700

    GroupHelper class created

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -9,14 +9,12 @@ def app(request):
     request.addfinalizer(fixture.destroy)
     return fixture
 
-
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="dfgh", header="dfgh", footer="dfghj"))
+    app.group.create(Group(name="dfgh", header="dfgh", footer="dfghj"))
     app.session.logout()
-
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
