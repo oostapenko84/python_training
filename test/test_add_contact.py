24cfefcb80a71aed0642cbaf7d951b0672672bd3
Olga Ostapenko <olga.ostapenko@leverx.com>
2016-05-31 08:07:59 -0700

    SessionHelper class created (sessions management)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -10,6 +10,6 @@ def app(request):
     return fixture
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(First_Name="Olga", Middle_Name="Sergeevna", Last_Name="Ostapenko", Company_Name="Test Company", Address="Test Address", Home_Phone="12345", Mobile_Phone="12345", Work_Phone="12345",Email="test@test.com", Homepage="www.test.com", Birthday="1984", Anniversary="2017", Secondary_Address="Test Address"))
-    app.logout()
+    app.session.logout()
